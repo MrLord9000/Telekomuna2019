483fe7642460323d844bb4dacd6baebdeae58aa2
MrLord9000 <filip1209348756@gmail.com>
2019-03-08 00:18:11 +0100

    Created BitMatrix class and first unit tests

---
+++ TelekomunikacjaZadanie1/TelekomunikacjaZadanie1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelekomunikacjaZadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BitMatrix testMatrix = new BitMatrix(6, 4);
+            testMatrix.Print();
+
+            Console.ReadKey();
+        }
+
+    }
+
+    public class BitMatrix
+    {
+        private BitArray[] bitArrays;
+
+        public BitMatrix(int rows, int columns, bool defaultValue = false)
+        {
+            bitArrays = new BitArray[rows];
+            for (int i = 0; i < rows; i++)
+            {
+                bitArrays[i] = new BitArray(columns, defaultValue);
+            }
+        }
+
+        public bool this[int i, int j]
+        {
+            get => bitArrays[i][j];
+            set => bitArrays[i][j] = value;
+        }
+
+        public BitArray GetRow(int row)
+        {
+            return bitArrays[row];
+        }
+
+        public void Print()
+        {
+            foreach(BitArray arr in bitArrays)
+            {
+                Console.Write("| ");
+                PrintRow(arr);
+                Console.WriteLine("|");
+            }
+        }
+
+        public void PrintRow(int row)
+        {
+            foreach(bool val in bitArrays[row])
+            {
+                Console.Write(val ? "1" : "0" + " ");
+            }
+        }
+
+        public void PrintRow(IEnumerable arr)
+        {
+            foreach (bool val in arr)
+            {
+                Console.Write(val ? "1" : "0" + " ");
+            }
+        }
+
+    }
+}
---
+++ TelekomunikacjaZadanie1/TelekomunikacjaZadanie1Tests/BitMatrixTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TelekomunikacjaZadanie1;
+using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelekomunikacjaZadanie1.Tests
+{
+    [TestClass()]
+    public class BitMatrixTests
+    {
+        [TestMethod()]
+        public void BitMatrixTest()
+        {
+            // Arrange =================================
+            BitMatrix testMatrix = new BitMatrix(10, 10);
+
+            // Act =====================================
+            testMatrix[0, 0] = true;
+            testMatrix[7, 4] = true;
+
+            // Assert ==================================
+            Assert.AreEqual(true, testMatrix[0, 0]);
+            Assert.AreEqual(true, testMatrix[7, 4]);
+            Assert.AreEqual(false, testMatrix[0, 1]);
+            Assert.AreEqual(false, testMatrix[1, 0]);
+        }
+
+        [TestMethod()]
+        public void GetRowTest()
+        {
+            // Arrange =================================
+            BitMatrix testMatrix = new BitMatrix(6, 4);
+            BitArray testArray = new BitArray(4);
+
+            // Act =====================================
+            testMatrix[0, 0] = true;
+            testMatrix[0, 1] = true;
+            testMatrix[0, 2] = true;
+
+            testArray[0] = true;
+            testArray[1] = true;
+            testArray[2] = true;
+
+            // Assert ==================================
+            Assert.AreEqual(testArray[0], testMatrix.GetRow(0)[0]);
+            Assert.AreEqual(testArray[1], testMatrix.GetRow(0)[1]);
+            Assert.AreEqual(testArray[2], testMatrix.GetRow(0)[2]);
+            Assert.AreEqual(testArray[3], testMatrix.GetRow(0)[3]);
+
+        }
+
+        [TestMethod()]
+        public void PrintTest()
+        {
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PrintRowTest()
+        {
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void PrintRowTest1()
+        {
+            //Assert.Fail();
+        }
+    }
+}

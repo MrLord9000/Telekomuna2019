97eba3e661ada56bef19fb3b7d6f2b9ae66dcba9
DESKTOP-NPUJM4H\fmaz9 <filip1209348756@gmail.com>
2019-03-12 15:43:51 +0100

    Matrix multiplication is working - but don't treat it as a universal method - only for our applications

---
+++ TelekomunikacjaZadanie1/TelekomunikacjaZadanie1/Exceptions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+[Serializable]
+public class TooLittleData : Exception
+{
+    public TooLittleData()
+    {
+    }
+
+    public TooLittleData(string message) : base(message)
+    {
+    }
+
+    public TooLittleData(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+
+    protected TooLittleData(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
+
+[Serializable]
+public class IncorrectMatrixSize : Exception
+{
+    public IncorrectMatrixSize()
+    {
+    }
+
+    public IncorrectMatrixSize(string message) : base(message)
+    {
+    }
+
+    public IncorrectMatrixSize(string message, Exception innerException) : base(message, innerException)
+    {
+    }
+
+    protected IncorrectMatrixSize(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
--- TelekomunikacjaZadanie1/TelekomunikacjaZadanie1/Program.cs
+++ TelekomunikacjaZadanie1/TelekomunikacjaZadanie1/Program.cs
@@ -16,17 +16,24 @@ namespace TelekomunikacjaZadanie1
     {
         static void Main(string[] args)
         {
-            BitMatrix testMatrix = new BitMatrix(6, 4);
+            BitMatrix testMatrix = new BitMatrix(7, 1);
+            testMatrix[0, 0] = false;
+            testMatrix[1, 0] = true;
+            testMatrix[2, 0] = false;
+            testMatrix[3, 0] = false;
+            testMatrix[4, 0] = true;
+            testMatrix[5, 0] = true;
+            testMatrix[6, 0] = false;
+            Console.WriteLine("TestMatrix:");
             testMatrix.Print();
 
-            Console.WriteLine();
-            GenerateCorrectionMatrix(11, 1).Print();
+            BitMatrix testCorrectionMatrix = GenerateCorrectionMatrix(4, 1);
+            Console.WriteLine("CorrectionMatrix:");
+            testCorrectionMatrix.Print();
 
-            Console.WriteLine();
-            GenerateCorrectionMatrix(15, 1).Print();
-
-            Console.WriteLine();
-            GenerateCorrectionMatrix(26, 1).Print();
+            BitMatrix outputMatrix = testCorrectionMatrix * testMatrix;
+            Console.WriteLine("OutputMatrix:");
+            outputMatrix.Print();
 
             Console.ReadKey();
         }
@@ -35,10 +42,10 @@ namespace TelekomunikacjaZadanie1
         {
             //This isn't necessary, but basically doing this for less than 4 bits of data is pointless
             //Remove this line if necessary
-            if (dataBits < 4) throw new TooLittleData();
+            //if (dataBits < 4) throw new TooLittleData();
 
-            int bitsSize = dataBits + 2;    //Creating variable for required code word size
-            int parityBits = 2;             //Creating variable for the parity bits amount
+            int bitsSize = dataBits + 1;    //Creating variable for required code word size
+            int parityBits = 1;             //Creating variable for the parity bits amount
 
             //Counting the amout of parityBits required for specified data length
             for (int i = 2; i <= bitsSize; i *= i)
@@ -93,7 +100,7 @@ namespace TelekomunikacjaZadanie1
             {
                 bitArrays[i] = new BitArray(columns, defaultValue);
             }
-        }
+        }        
 
         public bool this[int i, int j]
         {
@@ -101,9 +108,61 @@ namespace TelekomunikacjaZadanie1
             set => bitArrays[i][j] = value;
         }
 
+        public BitArray GetColumn(int column)
+        {
+            BitArray output = new BitArray(bitArrays.Length);
+            for (int i = 0; i < bitArrays.Length; i++)
+            {
+                output[i] = bitArrays[i][0];
+            }
+
+            return output;
+        }
+
         public BitArray GetRow(int row)
         {
             return bitArrays[row];
+        }
+
+        public int Rows()
+        {
+            return bitArrays.Length;
+        }
+
+        public int Columns()
+        {
+            return bitArrays[0].Length;
+        }
+
+        public void Transpose()
+        {
+            BitArray[] output = new BitArray[bitArrays[0].Length];
+            for (int i = 0; i < bitArrays[0].Length; i++)
+            {
+                output[i] = new BitArray(bitArrays.Length);
+                for (int j = 0; j < bitArrays.Length; j++)
+                {
+                    output[i][j] = bitArrays[j][i];
+                }
+            }
+            bitArrays = output;
+        }
+
+        public static BitMatrix operator*(BitMatrix matrixL, BitMatrix matrixP)
+        {
+            if(matrixL.Columns() != matrixP.Rows())
+            {
+                throw new IncorrectMatrixSize();
+            }
+            BitMatrix output = new BitMatrix(1, matrixL.Rows());
+            for (int i = 0; i < matrixL.Rows(); i++)
+            {
+                for (int j = 0; j < matrixL.Columns(); j++)
+                {
+                    if (matrixL[i, j] && matrixP[j, 0]) output[0, i] = !output[0, i];
+                }
+            }
+            return output;
         }
 
         public void Print()
@@ -133,24 +192,5 @@ namespace TelekomunikacjaZadanie1
         }
 
     }
-}
 
-[Serializable]
-internal class TooLittleData : Exception
-{
-    public TooLittleData()
-    {
-    }
-
-    public TooLittleData(string message) : base(message)
-    {
-    }
-
-    public TooLittleData(string message, Exception innerException) : base(message, innerException)
-    {
-    }
-
-    protected TooLittleData(SerializationInfo info, StreamingContext context) : base(info, context)
-    {
-    }
 }

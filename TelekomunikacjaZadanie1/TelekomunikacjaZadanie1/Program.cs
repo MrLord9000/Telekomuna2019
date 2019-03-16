32a69d38e8c085c1233d869a29bf4323851fabb6
MrLord9000 <filip1209348756@gmail.com>
2019-03-16 18:20:18 +0100

    Setting parity bits now works

--- TelekomunikacjaZadanie1/TelekomunikacjaZadanie1/Program.cs
+++ TelekomunikacjaZadanie1/TelekomunikacjaZadanie1/Program.cs
@@ -22,24 +22,29 @@ namespace TelekomunikacjaZadanie1
     {
         static void Main(string[] args)
         {
-            BitMatrix testMatrix = new BitMatrix(7, 1);
-            testMatrix[0, 0] = false;
+            BitMatrix testMatrix = new BitMatrix(8, 1);
+            testMatrix[0, 0] = true;
             testMatrix[1, 0] = false;
             testMatrix[2, 0] = true;
             testMatrix[3, 0] = false;
             testMatrix[4, 0] = false;
             testMatrix[5, 0] = false;
             testMatrix[6, 0] = true;
+            testMatrix[7, 0] = true;
             Console.WriteLine("TestMatrix:");
             testMatrix.Print();
 
-            BitMatrix testCorrectionMatrix = GenerateCorrectionMatrix(4, 1);
-            Console.WriteLine("CorrectionMatrix:");
-            testCorrectionMatrix.Print();
+            Console.WriteLine();
+            BitCorrection.oneError8bit.Print();
 
-            BitMatrix outputMatrix = testCorrectionMatrix * testMatrix;
-            Console.WriteLine("OutputMatrix:");
-            outputMatrix.Print();
+            Console.WriteLine();
+            BitCorrection.twoErrors8bit.Print();
+
+            Console.WriteLine();
+            BitCorrection.threeErrors8bit.Print();
+
+            Console.WriteLine();
+            BitCorrection.SetParity(BitCorrection.oneError8bit, testMatrix).Print();
 
             Console.ReadKey();
         }
@@ -95,6 +100,46 @@ namespace TelekomunikacjaZadanie1
         }
     }
 
+    public class BitCorrection
+    {
+        public static BitMatrix oneError8bit = new BitMatrix(new BitArray[4] { new BitArray( new bool[]{true, true, false, true, true, false, true, false, true, false, false, false } ),
+                                                                               new BitArray( new bool[]{true, false, true, true, false, true, true, false, false, true, false, false } ),
+                                                                               new BitArray( new bool[]{false, true, true, true, false, false, false, true, false, false, true, false} ),
+                                                                               new BitArray( new bool[]{false, false, false, false, true, true, true, true, false, false, false, true} ) } );
+
+        public static BitMatrix twoErrors8bit = new BitMatrix(new BitArray[5] { new BitArray( new bool[]{true, false, false, true, true, true, false, true, true, false, false, false, false } ),
+                                                                                new BitArray( new bool[]{true, true, false, true, true, false, true, false, false, true, false, false, false } ),
+                                                                                new BitArray( new bool[]{true, true, true, false, false, true, true, false, false, false, true, false, false } ),
+                                                                                new BitArray( new bool[]{false, true, true, true, false, false, false, true, false, false, false, true, false} ),
+                                                                                new BitArray( new bool[]{false, false, true, false, true, true, true, true, false, false, false, false, true } ) });
+
+        public static BitMatrix threeErrors8bit = new BitMatrix(new BitArray[6] { new BitArray( new bool[]{true, false, false, true, true, true, false, false, true, false, false, false, false, false } ),
+                                                                                  new BitArray( new bool[]{true, true, false, false, true, true, true, true, false, true, false, false, false, false } ),
+                                                                                  new BitArray( new bool[]{true, true, true, true, false, true, false, true, false, false, true, false, false, false } ),
+                                                                                  new BitArray( new bool[]{true, true, true, true, true, false, true, false, false, false, false, true, false, false } ),
+                                                                                  new BitArray( new bool[]{false, true, true, true, true, true, true, true, false, false, false, false, true, false } ),
+                                                                                  new BitArray( new bool[]{false, false, true, false, false, false, true, true, false, false, false, false, false, true } ) });
+
+        public static BitMatrix SetParity(BitMatrix correctionMatrix, BitMatrix message)
+        {
+            BitMatrix output = new BitMatrix(1, correctionMatrix.Columns());
+
+            if (message.Columns() > 1) message.Transpose();
+            for (int i = 0; i < message.Rows(); i++)
+            {
+                output[0, i] = message[i, 0];
+            }
+            output.Transpose();
+            BitMatrix temp = correctionMatrix * output;
+            output.Transpose();
+            for (int i = 0; i < temp.Columns(); i++)
+            {
+                output[0, i + 8] = temp[0, i];
+            }
+            return output;
+        }
+    }
+
     public class BitMatrix
     {
         private BitArray[] bitArrays;
@@ -109,7 +154,16 @@ namespace TelekomunikacjaZadanie1
             {
                 bitArrays[i] = new BitArray(columns, defaultValue);
             }
-        }        
+        }
+        
+        public BitMatrix(BitArray[] rows)
+        {
+            bitArrays = new BitArray[rows.Length];
+            for (int i = 0; i < rows.Length; i++)
+            {
+                bitArrays[i] = rows[i];
+            }
+        }
 
         public bool this[int i, int j]
         {
